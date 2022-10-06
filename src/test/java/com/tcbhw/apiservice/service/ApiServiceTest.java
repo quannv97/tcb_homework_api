86f3a54a945843e9041d139f48a18c5d9f3ef146
Quan Ngo Van <ngovanquan97vt@gmail.com>
2022-10-06 22:41:41 +0700

    add unit test

--- src/test/java/com/tcbhw/apiservice/service/ApiServiceTest.java
+++ src/test/java/com/tcbhw/apiservice/service/ApiServiceTest.java
@@ -1,6 +1,8 @@
 package com.tcbhw.apiservice.service;
 
 import com.tcbhw.apiservice.entity.PoolEntity;
+import com.tcbhw.apiservice.entity.QueryEntity;
+import com.tcbhw.apiservice.entity.QueryReturnEntity;
 import com.tcbhw.constant.CommonConstants;
 import org.junit.Assert;
 import org.junit.jupiter.api.Test;
@@ -13,11 +15,12 @@ class ApiServiceTest {
     @Autowired
     ApiInterface apiInterface;
 
+    //1st call createPool
     @Test
-    void createPoolInsertedSuccess1() {
+    void createPoolTest1() {
         //Create mock data
         Long poolId = 1L;
-        Long[] poolValues = {1L, 2L, 3L, 4L, 5L};
+        Long[] poolValues = {1L, 2L};
         PoolEntity poolEntity = new PoolEntity();
         poolEntity.setPoolId(poolId);
         poolEntity.setPoolValues(poolValues);
@@ -27,12 +30,13 @@ class ApiServiceTest {
         Assert.assertEquals(CommonConstants.INSERTED, result1);
     }
 
+    //2nd call createPool
     @Test
-    void createPoolInsertedSuccess2() {
+    void createPoolTest2() {
         //Create mock data
-        Long poolId1 = 1L;
-        Long poolId2 = 2L;
-        Long poolId3 = 3L;
+        Long poolId1 = 2L;
+        Long poolId2 = 3L;
+        Long poolId3 = 4L;
         Long[] poolValues1 = {1L, 2L};
         Long[] poolValues2 = {2L, 3L};
         Long[] poolValues3 = {4L, 5L};
@@ -42,9 +46,9 @@ class ApiServiceTest {
 
         //call service many time with other poolId and expect result is "INSERTED"
         String result1 = apiInterface.createPool(poolEntity);
+        Assert.assertEquals(CommonConstants.INSERTED, result1);
         poolEntity.setPoolId(poolId2);
         poolEntity.setPoolValues(poolValues2);
-        Assert.assertEquals(CommonConstants.INSERTED, result1);
         String result2 = apiInterface.createPool(poolEntity);
         Assert.assertEquals(CommonConstants.INSERTED, result2);
         poolEntity.setPoolId(poolId3);
@@ -53,32 +57,69 @@ class ApiServiceTest {
         Assert.assertEquals(CommonConstants.INSERTED, result3);
     }
 
+    //3rd call createPool
     @Test
-    void createPoolAppendSuccess() {
+    void createPoolTest3() {
         //Create mock data
-        Long poolId = 0L;
-        Long[] poolValues1 = {1L, 2L};
-        Long[] poolValues2 = {3L, 4L, 5L};
+        Long poolId = 1L;
+        Long[] poolValues1 = {3L, 4L, 5L};
         PoolEntity poolEntity = new PoolEntity();
         poolEntity.setPoolId(poolId);
         poolEntity.setPoolValues(poolValues1);
 
-        //call service first time and expect result is "INSERTED"
-        String result1 = apiInterface.createPool(poolEntity);
-        Assert.assertEquals(CommonConstants.INSERTED, result1);
-
-        //call service second time with the same poolId and expect result is "APPENDED"
-        poolEntity.setPoolValues(poolValues2);
-        String result2 = apiInterface.createPool(poolEntity);
-        Assert.assertEquals(CommonConstants.APPENDED, result2);
+        //call service expect result is "APPENDED"
+        String result = apiInterface.createPool(poolEntity);
+        Assert.assertEquals(CommonConstants.APPENDED, result);
     }
 
     @Test
     void query() {
+        //Create mock data
+        Long poolId = 5L;
+        Long[] poolValues = {1L, 2L, 3L, 4L, 5L};
+        Double percent = 50.0;
+        QueryEntity queryEntity = new QueryEntity();
+        queryEntity.setPoolId(poolId);
+        queryEntity.setPercentile(percent);
+
+        //createPool
+        PoolEntity poolEntity = new PoolEntity(poolId, poolValues);
+        apiInterface.createPool(poolEntity);
+
+        //call service query with poolId = 5 and poolValues [1,2,3,4,5], percentile=50% expect result is "3"
+        QueryReturnEntity queryReturnEntity = apiInterface.query(queryEntity);
+        Assert.assertEquals(3, queryReturnEntity.getCalculatedQuantile());
+
+        //call service query with poolId = 6 and poolValues [1,2,3,4,5], percentile=50% expect result "Pool is not existed!"
+        queryEntity.setPoolId(6L);
+        QueryReturnEntity queryReturnEntityNew = apiInterface.query(queryEntity);
+        Assert.assertEquals(null, queryReturnEntityNew);
     }
 
     @Test
     void getPool() {
+
+        //Create mock data
+        Long poolId1 = 1L;
+        Long poolId2 = 2L;
+        Long[] poolValues1 = {1L, 2L, 3L, 4L, 5L};
+        Long[] poolValues2 = {1L, 2L, 5L};
+
+        //createPool
+        PoolEntity poolEntity1 = new PoolEntity(poolId1, poolValues1);
+        apiInterface.createPool(poolEntity1);
+        PoolEntity poolEntity2 = new PoolEntity(poolId2, poolValues2);
+        apiInterface.createPool(poolEntity2);
+
+        //call service with poolId = 1 and 2 expect PoolEntity not null
+        PoolEntity resultPoolEntity1 = apiInterface.getPool(1L);
+        Assert.assertEquals(1, resultPoolEntity1.getPoolId());
+        PoolEntity resultPoolEntity2 = apiInterface.getPool(2L);
+        Assert.assertEquals(2, resultPoolEntity2.getPoolId());
+
+        //call service with poolId = 10 expect PoolEntity is null
+        PoolEntity poolEntity10 = apiInterface.getPool(10L);
+        Assert.assertEquals(null, poolEntity10);
     }
 
     @Test
